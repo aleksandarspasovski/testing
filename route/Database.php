c3b7135c6c6a4d56e0f361727cb822f55a4638ed
Aleksandar Spasovski <48162180+aleksandarspasovski@users.noreply.github.com>
2020-10-01 11:41:42 +0200

    Update Database.php

--- route/Database.php
+++ route/Database.php
@@ -5,7 +5,7 @@ class Database
 	public $db;
 	function __construct()
 	{
-        $db = new mysqli('localhost', 'root', 'homosapiens', 'testBoolean', '3308');
+        $db = new mysqli('localhost', 'root', '', 'testBoolean', '3308');
 		$this->db = $db;
     }
 }
